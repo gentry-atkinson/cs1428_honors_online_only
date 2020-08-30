73dbaece00ff353c030a79ebe81f7d059e817d4d
Gentry Atkinson <gma23@txstate.edu>
2020-08-30 14:18:29 -0500

    writing sample code for lab 1

---
+++ lab1/sample_lab1.cpp
@@ -0,0 +1,21 @@
+//Author:
+//Date:
+//Class: CS1428 HL1
+//Description: This program will create a student ID number and save a student's
+//    information in a separate file.
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+//This function will print the number of a letter from the alphabet
+//A = 1, B = 2, C = 3, and so on...
+int numberOfCharacter(char a){
+    return int(a) - 'A' + 1;
+}
+
+int main(){
+    string firstName;
+
+}
