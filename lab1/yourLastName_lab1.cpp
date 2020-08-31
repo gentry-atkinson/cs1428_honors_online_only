943ffb1c0c80ab07cdcf8f29a4e0b1b7e9ba3d34
Gentry Atkinson <gma23@txstate.edu>
2020-08-30 21:49:21 -0500

    created empty lab 1 program

---
+++ lab1/yourLastName_lab1.cpp
@@ -0,0 +1,31 @@
+//Author:
+//Date:
+//Class: CS1428 HL1
+//Description: This program will create a student ID number and save a student's
+//    information in a separate file.
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+//This function will print the number of a letter from the alphabet
+//A = 1, B = 2, C = 3, and so on...
+int numberOfCharacter(char a){
+    return int(a) - 'A' + 1;
+}
+
+int main(){
+    //Define your variables here
+
+    //Print questions and take answers here
+
+    //open the filestream with a unique name
+    outFile.open(firstName + ".txt");
+
+    //write the answers to the file
+
+    //generate and print the ID number on the console
+
+    return 0;
+}
