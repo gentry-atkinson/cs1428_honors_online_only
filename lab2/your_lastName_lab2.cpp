28fea0229375533423bf0ab72867ff4354f8c64b
Gentry Atkinson <gma23@txstate.edu>
2020-09-12 17:32:39 -0500

    made blank code file

--- lab2/your_lastName_lab2.cpp
+++ lab2/your_lastName_lab2.cpp
@@ -11,51 +11,25 @@ using namespace std;
 
 int main(){
     //get user input and store in appropriate variables
-    string name;
-    float gpa;
-    int act, zip;
     cout << "What is this students last name: ";
-    cin >> name;
+
     cout << "What is this students GPA: ";
-    cin >> gpa;
+
     cout << "How many extracurricular activities: ";
-    cin >> act;
+
     cout << "What ZIP code is this student’s address in: ";
-    cin >> zip;
+
 
     //open the output file stream in append mode using open("accepted.txt", std::fstream::app)
-    ofstream outFile;
-    outFile.open("accepted.txt", std::fstream::app);
+
 
     //adjust gpa using extracurricular activities
-    if (act > 3) gpa += 0.3;
-    else gpa += (0.1 * act);
+
 
     //print "accept", "wait list", or "do not accept" using adjusted gpa
     //this output should be on the console
     //if the ourput is accept, write the name to accepted.txt on its own line
 
-    if (zip == 78666){
-        if (gpa > 3.5) {
-            cout << "accept" << endl;
-            outFile << name << endl;
-        }
-        else if(gpa > 3.2)
-            cout << "wait list" << endl;
-        else
-            cout << "do not accept" << endl;
-
-    }
-    else{
-        if (gpa > 3.7) {
-            cout << "accept" << endl;
-            outFile << name << endl;
-        }
-        else if(gpa > 3.4)
-            cout << "wait list" << endl;
-        else
-            cout << "do not accept" << endl;
-    }
 
 
     return 0;
