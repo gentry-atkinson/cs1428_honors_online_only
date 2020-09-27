09e68422988f97af1f926a685b43aa49119304f7
Gentry Atkinson <gma23@txstate.edu>
2020-09-27 14:42:25 -0500

    created student file for program 4

---
+++ lab4/yourLastName_lab4.cpp
@@ -0,0 +1,59 @@
+//Author:
+//Date:
+//Class: CS1428 HL1
+//Description: This program will collect grades for one assignment
+
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+//Q1: create a constant integer named CLASS_SIZE
+const int CLASS_SIZE = 10;
+
+int main(){
+    //Q2: ask the user to enter a class title
+
+    //Q3: open an output filestream using the class title
+
+    //Q4: create an array with size CLASS_SIZE to hold the grades
+
+    //Q5: create variable to store a minimum grade, a maximum grade, and the
+    //  sum of all of the grades. Initialize all of the variables.
+
+    //Q6: create a for loop
+    //  Q6a: initialize a variable i=0;
+    //  Q6b: run while i is less than CLASS_SIZE
+    //  Q6c: increase i by 1 every time the loop runs
+    for(){
+        //Q7a: prompt the user to enter a grade using a do...while loop
+
+        //Q7b: store the new grade in the file
+
+    }
+
+    //Q8: create a while loop
+    //  Q8a: initialize a variable i=0;
+    //  Q8b: run while i is less than CLASS_SIZE
+    //  Q8c: increase i by 1 every time the loop runs
+
+    while(){
+        //Q9a: if grades[i] is the smallest processed yet, update the minimum variable
+
+        //Q9b; if grades[i] is the largest processed yet, update the maximum variable
+
+        //Q9c: add grades[i] into the sum variable
+
+    }
+
+    //Q10a: print the maximum grade
+    cout << "The maximum grade is: ";
+    //Q10b: print the minimum grade
+    cout << "The minimum grade is: ";
+    //Q10c: print the average grade
+    cout << "The average grade is: ";
+
+    //Q11: close your output file and print a goodbye message
+
+    return 0;
+}
