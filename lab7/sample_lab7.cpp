3c7f171a60fe2b5c8fab880b9ed9ce53a08f38c6
Gentry Atkinson <gma23@txstate.edu>
2020-10-18 11:27:25 -0500

    doing sample file for lab 7

---
+++ lab7/sample_lab7.cpp
@@ -0,0 +1,234 @@
+//Author:
+//Date:
+//Class: CS1428 HL1
+//Description: This program will connect four code modules to make a convenient
+//      program for school administration.
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+const int CLASS_SIZE = 10;
+
+//Generate ID
+//Parameters: none
+//Returns: the student's ID number
+//Side effects: none
+string generate_ID();
+
+//Q1: Define your facutlyMember struct here
+struct facultyMember{
+    string firstName;
+    string lastName;
+    string title;
+    string department;
+};
+
+const int FACULTY_SIZE = 20;
+
+char accepted();
+void build_catalog(ofstream&);
+void grade_book(float[]);
+
+int
+
+int main(){
+    ofstream outFile("catalog.txt", std::fstream::app);
+
+    char input;
+    float grades[CLASS_SIZE];
+    string studentID;
+    facultyMember roster[FACULTY_SIZE];
+
+    //Q2: create a loop that will run until the user chooses to quit
+    do{
+        //Q3: print the menu and accept the user's choice
+        cout << "i- generate an ID" << endl;
+        cout << "a- determine if a student is accepted" << endl;
+        cout << "c- enter a course in the catalog" << endl;
+        cout << "g- print the max, min, and avg grade of an assignment" << endl;
+        cout << "q- quit the program" << endl;
+        cout << "Please select one option from the menu:";
+        cin >> input;
+
+        //Q4: branch to call the correct function
+        switch(input){
+            //Q5: choice is 'i'
+            case 'i':
+                studentID = generate_ID();
+                cout << "Student ID: " << studentID << endl;
+                break;
+            //Q6: choice is 'a'
+            case 'a':
+                switch(accepted()){
+                    case 'a':
+                        cout << "accepted" << endl;
+                        break;
+                    case 'w':
+                        cout << "wait list" << endl;
+                        break;
+                    case 'n':
+                        cout << "not accepted" << endl;
+                        break;
+                }
+                break;
+            //Q7: choice is 'c'
+            case 'c':
+                build_catalog(outFile);
+                break;
+            //Q8: choice is 'g'
+            case 'g':
+                for(int i = 0; i < CLASS_SIZE; i++){
+                    cout << "Enter one grade: ";
+                    cin >> grades[i];
+                }
+                grade_book(grades);
+                break;
+            //Q9: choice is 'q'
+            case 'q':
+                cout << "Thank you for using this program." << endl;
+                break;
+            //Q10: print an error if the user chooses an option that is not on the menu
+            default:
+                cout << "That is not a valid selection." << endl;
+                break;
+        }
+    }while(input!='q');
+
+    //Q11: close the filestream
+    outFile.close();
+    return 0;
+}
+
+int numberOfCharacter(char a){
+    return int(a) - 'A' + 1;
+}
+
+string generate_ID(){
+    //Define your variables here
+    string firstName;
+    char lastInitial;
+    int birthYear;
+    char fallOrSpring;
+    int last4;
+
+    //Print questions and take answers here
+    cout << "What is the student's first name: ";
+    cin >> firstName;
+    cout << "What is the first letter of the last name (capitalized): ";
+    cin >> lastInitial;
+    cout << "What year was the student born in: ";
+    cin >> birthYear;
+    cout << "Will the student start school in the Fall or the Spring (F or S): ";
+    cin >> fallOrSpring;
+    cout << "What are the last 4 digits of the student's social: ";
+    cin >> last4;
+
+    //generate and print the ID number on the console
+    int group1 =  numberOfCharacter(lastInitial)+numberOfCharacter(fallOrSpring);
+    int group2 =  birthYear + (last4%1337);
+
+    return to_string(group1) + "-" + to_string(group2);
+}
+
+char accepted(){
+    //get user input and store in appropriate variables
+    float gpa;
+    int act, zip;
+    cout << "What is this students GPA: ";
+    cin >> gpa;
+    cout << "How many extracurricular activities: ";
+    cin >> act;
+    cout << "What ZIP code is this student’s address in: ";
+    cin >> zip;
+
+    //adjust gpa using extracurricular activities
+    if (act > 3) gpa += 0.3;
+    else gpa += (0.1 * act);
+
+    //print "accept", "wait list", or "do not accept" using adjusted gpa
+    //this output should be on the console
+    //if the ourput is accept, write the name to accepted.txt on its own line
+
+    if (zip == 78666){
+        if (gpa > 3.5) {
+            return 'a';
+        }
+        else if(gpa > 3.2)
+            return 'w';
+        else
+            return 'n';
+
+    }
+    else{
+        if (gpa > 3.7) {
+            return 'a';
+        }
+        else if(gpa > 3.4)
+            return 'w';
+        else
+            return 'n';
+    }
+}
+
+void build_catalog(ofstream &outFile){
+    string prefix;
+    int cNumber, cYear;
+    char semester, pfOrL, saveFile;
+    //course prefix
+    //Valid answers: "CS", "LANG", or "ECE"
+    do{
+        cout << "What is the course prefix(CS, LANG, or ECE):";
+        cin >> prefix;
+    }while(prefix != "CS" && prefix != "LANG" && prefix != "LANG");
+
+    //course number
+    //Valid answers: 1000-4999
+    do{
+        cout << "What is the course number(1000-4999): ";
+        cin >> cNumber;
+    }while(cNumber < 1000 || cNumber > 4999);
+
+    //semester
+    //Valid answers: F, S
+    do{
+        cout << "What semester is this course taught(F or S): ";
+        cin >> semester;
+    }while(semester != 'F' && semester != 'S');
+
+    //course year
+    //Valid answers: 2020+
+    cYear = 0;
+    while(cYear < 2020){
+        cout << "What year will this course be taught(2020 or later): ";
+        cin >> cYear;
+    }
+
+    //pass/fail or letter
+    //Valid answers: P, L
+    pfOrL = 'a';
+    while(pfOrL != 'P' && pfOrL != 'L'){
+        cout << "Pass/fail or letter grade (P or L): ";
+        cin >> pfOrL;
+    }
+    cout << endl;
+    //write to file
+    outFile << prefix << "-" << cNumber << ", " << semester << " " << cYear << ", " << pfOrL << endl;
+}
+
+void grade_book(float studentGrades[]){
+    int maximum = -1, minimum = 101;
+    float total = 0.0;
+    for(int i = 0; i < CLASS_SIZE; i++){
+        //if grades[i] is the smallest processed yet, update the minimum variable
+        if(studentGrades[i] < minimum) minimum = studentGrades[i];
+        //if grades[i] is the largest processed yet, update the maximum variable
+        if(studentGrades[i] > maximum) maximum = studentGrades[i];
+        //add grades[i] into the sum variable
+        total += studentGrades[i];
+    }
+
+    cout << "The maximum grade is: " << maximum << endl;
+    cout << "The minimum grade is: " << minimum << endl;
+    cout << "The average grade is: " << total/CLASS_SIZE << endl;
+}
