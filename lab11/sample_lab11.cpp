296cf9c2a5a47bb8cbfffa6a14a62810a2acd34e
Gentry Atkinson <gma23@txstate.edu>
2020-11-15 09:38:57 -0600

    finished bad code for 11

--- lab11/sample_lab11.cpp
+++ lab11/sample_lab11.cpp
@@ -1,100 +1,97 @@
 //Author:
 //Date:
 //Class: CS1428 HL1
-//Description: This program will time and test two search algorithms using parallelism
+//Description: This program will initialize a random array and print some statistics about it
 #include<iostream>
 #include<cstdlib>
 
 using namespace std;
-using namespace std::chrono;
 
-const int NUMBER_OF_CORES = 4;
+int findMaxValue(int arr[], int s);
+int findMinValue(const int arr[], int s);
+long findSum(const int arr[], int s);
 
-//Fill in the definition of calcEfficiency
-int seqSearch(const int[], int, int);
-int parSearch(const int[], int, int);
-float calcEfficiency(float, float);
 
 //### Do not make any changes to the Main function###
 int main(){
 
-  const int LIST_SIZE = 100000;
-  int list[LIST_SIZE];
+  const int LIST_SIZE = 10000;
+  int arr[LIST_SIZE];
 
   //Assign a random value to every entry in the list and then make sure that
   //the search term is included in the list somewhere
   srand(23);
-  for(int i = 0; i < LIST_SIZE; i++) list[i] = rand();
+  for(int i = 0; i < LIST_SIZE; i++) arr[i] = rand();
+  //for(int i = 0; i < LIST_SIZE; i++) cout << arr[i] << " ";
+  //cout << endl;
 
-  //time a run of sequential search
-  auto start = high_resolution_clock::now();
-  int seqFound = seqSearch(list, LIST_SIZE, SEARCH_VALUE);
-  auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop - start);
-  float seqDuration = static_cast<float>(duration.count());
-
-  //time a run of parallel search
-  start = high_resolution_clock::now();
-  int parFound = parSearch(list, LIST_SIZE, SEARCH_VALUE);
-  stop = high_resolution_clock::now();
-  duration = duration_cast<microseconds>(stop - start);
-  float parDuration = static_cast<float>(duration.count());
-
-  //print results
-  cout << "Sequential search found the value " << SEARCH_VALUE << " at index "
-    << seqFound << " in " << seqDuration << " microseconds" << endl;
-
-  cout << "Parallel search found the value " << SEARCH_VALUE << " at index "
-    << parFound << " in " << parDuration << " microseconds" << endl;
-  cout << "The efficiency of the parallel algorithm is: " << calcEfficiency(seqDuration, parDuration)
-    << endl;
+  cout << "The smallest value in the list is: " << findMinValue(arr, LIST_SIZE) << endl;
+  cout << "The largest value in the list is: " << findMaxValue(arr, LIST_SIZE) << endl;
+  cout << "The sum of the values in the list is: " << findSum(arr, LIST_SIZE) << endl;
 
   return 0;
 }
 
-//Sequential Search
+//Find Max Value
 //Parameters:
-//  arr: the list to search
-//  s: the size of the list arr
-//  value: the term to search for in arr
-//Return: an index with value in arr
-//Side Effects:none
-//###Do not make changes to this function###
-int seqSearch(const int arr[], int s, int value){
-  int foundAt = -1;
-
-  for(int i = 0; i < s; i++){
-        if(arr[i] == value) foundAt = i;
-  }
-
-  return foundAt;
+//  arr:    an array of integers
+//  s:      the size of arr
+//Returns: the largest value in arr
+//Side Effects: none
+int findMaxValue(int arr[], int s){
+    int maxIndex = 0;
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < s; j++){
+            if(arr[j] > arr[maxIndex]) maxIndex = j;
+        }
+    }
+    return arr[maxIndex];
 }
 
-//Parallel Search
+//Find Min Value
 //Parameters:
-//  arr: the list to search
-//  s: the size of the list arr
-//  value: the term to search for in arr
-//Return: an index with value in arr
-//Side Effects:none
-//###Do not make changes to this function###
-int parSearch(const int arr[], int s, int value){
-  int foundAt = -1;
-  omp_set_num_threads(NUMBER_OF_CORES);
+//  arr:    an array of integers
+//  s:      the size of arr
+//Returns: the smallest value in arr
+//Side Effects: none
+int findMinValue(const int arr[], int s){
+    int arr_copy[s];
+    for(int i = 0; i < s; i++)arr_copy[i] = arr[i];
 
-  #pragma omp parallel for
-  for(int i = 0; i < s; i++){
-        if(arr[i] == value) foundAt = i;
-  }
-
-  return foundAt;
+    bool done = false;
+    int temp, maxVal=s-1;
+    while(!done){
+        done = true;
+        for(int i = 0; i<maxVal; i++){
+            if(arr_copy[i] > arr_copy[i+1]){
+                temp = arr_copy[i];
+                arr_copy[i] = arr_copy[i+1];
+                arr_copy[i+1] = temp;
+                done = false;
+            }
+        }
+        maxVal--;
+    }
+    return arr_copy[0];
 }
 
-//Calculate Efficiency
-//Parameters:
-//
-//Return:
-//Side Effects:
-float calcEfficiency(float seqRunTime, float parRunTime){
-    return (seqRunTime/parRunTime)/NUMBER_OF_CORES;
+//Just a helper function
+void add(long &a, int b){
+    a = a+b;
+    return;
 }
+
+//Find Sum
+//Parameters:
+//  arr:    an array of integers
+//  s:      the size of arr
+//Returns: the sum of all values in arr
+//Side Effects: none
+long findSum(const int arr[], int s){
+    long total = 0;
+    for(int i = 0; i < s; i++){
+        add(total, arr[i]);
+    }
+    return total;
+}
+
